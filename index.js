938c9362c2f7f9699ac07daaa108408734a4255a
0x4all <steven.2018@qq.com>
2018-10-05 20:14:33 +0800

    add foreachhandler

--- index.js
+++ index.js
@@ -27,6 +27,46 @@ sutil.foreachdir = function(rootdir, filename, func) {
     })
 }
 
+
+/**
+ * 1.执行 next(),后续handler继续执行
+ * 2.执行 next(err),后续不再执行，就进入 errhandler
+ * 3.只要不执行next，后续也不再执行
+ */
+sutil.foreachhandler = function(arr, req, errhandler) {
+    if(!(arr instanceof Array)) {
+        throw new Error("arr is not Array");
+        return;
+    }
+    if(!(errhandler && typeof errhandler == "function")) {
+        throw new Error("errhandler is not function");
+        return;
+    }
+    var i = 0;
+    var n = arr.length;
+    var next = function(err) {
+        if(err) {
+            errhandler(err);
+            return;
+        }
+        if(i < n) {
+            var handler = arr[i++];
+            if(!!handler && typeof handler === "function") {
+                handler(req, next);
+            }
+            else {
+                console.warn("array index:",i-1," is not function. ignored.")
+            }
+        }
+        else if(i == n){
+            errhandler(null, req);
+            return;
+        }
+    }
+    next();
+}
+
+
 //设置跨域访问
 sutil.crossdomain = function(app) {
     app.all('*', function(req, res, next) {
